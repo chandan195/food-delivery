4f94383afea84bda9847960c94cc461afef4a65f
@chandan195 <chandanmariya98@gmail.com>
2024-05-20 17:00:07 +0530

    make navbar with style

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 // import React from 'react'
 import "./Navbar.css"
+import {assets} from "../../assets/assets"
+import { useState } from "react"
 const Navbar = () => {
+    const [menu ,setMenu] =useState("home");
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <img src={assets.logo}  alt="logo" className="logo"/>
+        <ul className="navbar-menu">
+            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
+            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+            <li  onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact-us</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="search" />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="search"/>
+                <div className="dot"></div>
+            </div>
+            <button>sign in</button>
+        </div>
+    </div>
   )
 }
 
