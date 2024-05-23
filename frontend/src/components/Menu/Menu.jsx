aa053ced6d8e31c6cca0a64a9c4114f943958d1e
@chandan195 <chandanmariya98@gmail.com>
2024-05-23 16:27:31 +0530

    make menu seleted item

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Menu.css";
 import { menu_list } from "../../assets/assets";
-const Menu = () => {
+const Menu = ( {category ,setCategory}) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
@@ -13,8 +13,9 @@ const Menu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} alt={item.menu_name} className="" />
+            <div
+             onClick={()=>setCategory(prev=>prev===item.menu_name ?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt={item.menu_name}  />
               <p>{item.menu_name}</p>
             </div>
           );
