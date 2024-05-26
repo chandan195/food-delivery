4809013da5c732b7d40f23b81451d2c44e0ccfc9
@chandan195 <chandanmariya98@gmail.com>
2024-05-26 20:30:20 +0530

    make add button & counter botton

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 const FoodItem = ({ id, name, price, description, image }) => {
-  console.log(name);
+  // console.log(name);
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img src={image} alt={name} className="food-item-image" />
+        <img src={image} alt="add" className="food-item-image" />
+        {!itemCount ? (
+          <img
+            src={assets.add_icon_white}
+            onClick={() => setItemCount((prev) => prev + 1)}
+            alt="icon"
+            className="add"
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              src={assets.remove_icon_red}
+              alt="remove-icon"
+              className="food-item"
+              onClick={() => setItemCount((prev) => prev - 1)}
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt="add-icon"
+              className="food-item"
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
