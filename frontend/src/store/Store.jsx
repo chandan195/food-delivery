565138a508d9018524102895f80a2ced1b70185a
@chandan195 <chandanmariya98@gmail.com>
2024-05-30 17:46:54 +0530

    make play store and app-store icon

--- frontend/src/store/Store.jsx
+++ frontend/src/store/Store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AllProductSlice from "./slice/AllProductSlice";
+import Card from "./slice//CardSlice";
 const store = configureStore({
   reducer: {
     allProduct: AllProductSlice,
+    card: Card
   },
 });
 export default store;
