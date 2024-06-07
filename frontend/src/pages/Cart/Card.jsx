8380e026adeaa3aecc97e5205a9cf12f2474851d
@chandan195 <chandanmariya98@gmail.com>
2024-06-07 16:54:09 +0530

    add card page

--- frontend/src/pages/Cart/Card.jsx
+++ frontend/src/pages/Cart/Card.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
+import { useSelector } from "react-redux";
 const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+  const cardItems = useSelector((state) => state.card.Card);
+  console.log(cardItems);
 
-export default Card
+  return (
+    <div className="card">
+      <div className="card-item">
+        <div className="card-items-title">
+          <p>Item</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {cardItems.map((item, index) => {
+          return (
+            <div key={index}>
+              <div  className="card-items-title card-items-item">
+                <img src={item.image} alt={item.name} />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{item.qty}</p>
+                <p>{item.price * item.qty}</p>
+                <p className="cross">X</p>
+              </div>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
