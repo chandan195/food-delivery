99bd9343b6418b0be808df8f275d6784bf83a37e
@chandan195 <chandanmariya98@gmail.com>
2024-06-16 11:05:49 +0530

    add & remove feature in admin panel

--- admin-food-delivery/src/pages/listItems/ListItems.jsx
+++ admin-food-delivery/src/pages/listItems/ListItems.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-import "./ListItems.css"
+import React, { useEffect, useState } from "react";
+import "./ListItems.css";
+import axios from "axios";
+import {toast} from "react-toastify";
 const ListItems = () => {
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if (response.data.success) {
+      console.log(response.data);
+      
+      setList(response.data.data)
+    } else {
+      console.log("error");
+    }
+  };
+  const removeFood =async(foodId)=>{
+    // console.log(foodId)
+    const response = await axios.post(`${url}/api/food/remove`,{id:foodId});
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+      
+    }else{
+      toast.error("error");
+    }
+  }
+  useEffect(() => {
+    fetchList();
+  }, []);
   return (
-    <div>ListItems</div>
-  )
-}
+    <div className="list add flex-col">
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img  src={`${url}/images/`+item.image} alt="image" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={()=>removeFood(item._id)}className="curser">X</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default ListItems
+export default ListItems;
